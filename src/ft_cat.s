f36c067fd22f7caf8ef3b8d61d33c474a14203c7
freccia <Freccia@users.noreply.github.com>
2018-02-02 23:10:59 +0100

    ft_cat further cleaning

--- src/ft_cat.s
+++ src/ft_cat.s
@@ -6,7 +6,7 @@
 ;    By: lfabbro <lfabbro@student.42.fr>            +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2018/02/02 16:08:04 by lfabbro           #+#    #+#              ;
-;    Updated: 2018/02/02 22:57:05 by lfabbro          ###   ########.fr        ;
+;    Updated: 2018/02/02 23:09:08 by lfabbro          ###   ########.fr        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
@@ -14,22 +14,21 @@
 %define STDOUT				1
 %define READ				3
 %define WRITE				4
+%define BUFSIZE				512
 
 global		_ft_cat
 
 section		.data
 buf:
-	.ptr	dd  "                                                            "
-	.len	equ $ - buf.ptr
+	.ptr	resb BUFSIZE
 
 section		.text
 _ft_cat:
-	mov		rdx, buf.len	; reading buf.len bytes
-	lea		rsi, [rel buf.ptr]	; storing into buffer
-	push	rdx				; saving len from being destroyed by read and write
+	lea		rsi, [rel buf.ptr]	; loading buffer address
 
 .loop:
 	push	rdi				; rdi == fd
+	mov		rdx, BUFSIZE	; reading BUFSIZE bytes
 	mov		rax, MACH_SYSCALL(READ)	;
 	syscall					;
 	cmp		rax, 0			; (rax <= 0)
@@ -39,8 +38,6 @@ _ft_cat:
 	mov		rax, MACH_SYSCALL(WRITE) ;
 	syscall					;
 	pop		rdi				; rdi = fd
-	pop		rdx				; rdx = buf.len
-	push	rdx				; rdx == buf.len
 	jmp		.loop			;
 
 .end:
